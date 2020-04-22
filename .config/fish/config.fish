28fcfd3f24eb02c40d8e2d2403e415e80228c590
hisbaan <34548959+hisbaan@users.noreply.github.com>
2020-04-22 11:15:49 -0400

    Added espanso config, edited xinitrc, edited polybar, edited env variables, edited update-theme script.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,9 +4,9 @@ abbr --add sp 'sudo pacman'
 set -g theme_powerline_fonts yes
 set -g theme_nerd_fonts yes
 
-set -x PATH $PATH:/home/hisbaan/.local/bin/:/home/hisbaan/.local/bin/color-scripts/:/home/hisbaan/.local/bin/xresources/
-set -x EDITOR '/usr/bin/nvim'
-set -x DISPLAY :0
+set -x -g PATH $PATH:/home/hisbaan/.local/bin/:/home/hisbaan/.local/bin/color-scripts/:/home/hisbaan/.local/bin/xresources/
+set -x -g EDITOR '/usr/bin/nvim'
+set -x -g DISPLAY ':0'
 
 #sudo !!
 function sudo
