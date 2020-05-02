c140b3beabf9e806f7fe1105e040822f67259845
hisbaan <34548959+hisbaan@users.noreply.github.com>
2020-05-02 18:37:18 -0400

    Added rofi theme that is edited to match dracula. Make other minor edits.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,12 +7,3 @@ set -g theme_nerd_fonts yes
 set -x -g PATH $PATH:/home/hisbaan/.local/bin/:/home/hisbaan/.local/bin/color-scripts/:/home/hisbaan/.local/bin/xresources/
 set -x -g EDITOR '/usr/bin/nvim'
 set -x -g DISPLAY ':0'
-
-#sudo !!
-function sudo
-    if test "$argv" = !!
-        eval command sudo $history[1]
-    else
-        command sudo $argv
-    end
-end
--- .config/fish/functions/!!.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function !! --description 'alias !!=clear'
-	clear  $argv;
-end
