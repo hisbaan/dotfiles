14e26845505e9cdc9838dc6e984cb0b9650799e0
Hisbaan <34548959+hisbaan@users.noreply.github.com>
2020-06-23 10:24:23 -0400

    Switched to bspwm and added appropriate configs

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Terminate already running bar instances
 killall -q polybar
@@ -7,6 +7,4 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar mainbar-i3 &
-
-echo "Polybar launched..."
+polybar mainbar-bspwm &
