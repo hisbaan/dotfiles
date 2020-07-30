f3864769249280b0bdf45b66030616ed808e1948
hisbaan <34548959+hisbaan@users.noreply.github.com>
2020-07-29 23:23:56 -0400

    Added emacs dotfiles

--- .config/fish/functions/celluloid.fish
+++ .config/fish/functions/celluloid.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
 function celluloid --wraps='~/.config/i3/i3-swallow celluloid' --description 'alias celluloid ~/.config/i3/i3-swallow celluloid'
-  ~/.config/i3/i3-swallow celluloid $argv;
+  devour.sh celluloid $argv;
 end
--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,6 +7,4 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar mainbar-i3 &
-
-echo "Polybar launched..."
+polybar mainbar-bspwm &
