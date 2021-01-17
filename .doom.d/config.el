5458f1feb01ed14ec614302e7f8131f983eda556
hisbaan <34548959+hisbaan@users.noreply.github.com>
2021-01-16 23:55:00 -0500

    Removed mu4e -- wasn't really using it

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -55,7 +55,7 @@
 ;;;;;;;;;;;;;;
 
 (setq org-directory "~/Documents/org/"
-      org-latex-packages-alist '(("margin=0.5in" "geometry" nil))
+      org-latex-packages-alist '(("margin=1in" "geometry" nil))
       org-hide-emphasis-markers t)
 
 ;; Use minted instead of verbatim to allow for syntax hilighting in LaTeX export
@@ -75,7 +75,6 @@
  'org-babel-load-languages
  '((R . t)
    (latex . t)))
-
 ;;;;;;;;;;;
 ;; LaTeX ;;
 ;;;;;;;;;;;
@@ -113,54 +112,33 @@
 ;; MU4E ;;
 ;;;;;;;;;;
 
-(use-package mu4e
-  :ensure nil
-  ;; :load-path "/usr/share/emacs/site-lisp/mu4e/"
-  ;; :defer 20 ; Wait until 20 seconds after startup
-  :config
-
-  ;; This is set to 't' to avoid mail syncing issues when using mbsync
-  (setq mu4e-change-filenames-when-moving t)
-
-  ;; Refresh mail using isync every 10 minutes
-  (setq mu4e-update-interval (* 10 60))
-  (setq mu4e-get-mail-command "mbsync -c '/home/hisbaan/.config/mu4e/mbsyncrc' -a")
-  ;; (setq mu4e-maildir "~/Mail/hisbaan-gmail")
-  (setq mu4e-root-maildir "~/Mail/hisbaan-gmail")
-  (setq my-mu4e-account-alist "hisbaan-gmail")
-
-  (setq mu4e-drafts-folder "/[Gmail]/Drafts")
-  (setq mu4e-sent-folder   "/[Gmail]/Sent Mail")
-  (setq mu4e-refile-folder "/[Gmail]/All Mail")
-  (setq mu4e-trash-folder  "/[Gmail]/Trash")
-
-  (setq mu4e-maildir-shortcuts
-      '(("/Inbox"             . ?i)
-        ("/[Gmail]/Sent Mail" . ?s)
-        ("/[Gmail]/Trash"     . ?t)
-        ("/[Gmail]/Drafts"    . ?d)
-        ("/[Gmail]/All Mail"  . ?a))))
-
-;; (defun my-mu4e-set-account ()
-;;   "Set the account for composing a message."
-;;   (let* ((account
-;;           (if mu4e-compose-parent-message
-;;               (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
-;;                 (string-match "/\\(.*?\\)/" maildir)
-;;                 (match-string 1 maildir))
-;;             (completing-read (format "Compose with account: (%s) "
-;;                                      (mapconcat #'(lambda (var) (car var))
-;;                                                 my-mu4e-account-alist "/"))
-;;                              (mapcar #'(lambda (var) (car var)) my-mu4e-account-alist)
-;;                              nil t nil nil (caar my-mu4e-account-alist))))
-;;          (account-vars (cdr (assoc account my-mu4e-account-alist))))
-;;     (if account-vars
-;;         (mapc #'(lambda (var)
-;;                   (set (car var) (cadr var)))
-;;               account-vars)
-;;       (error "No email account found"))))
-;;
-;; (add-hook 'mu4e-compose-pre-hook 'my-mu4e-set-account)
+;; (use-package mu4e
+;;   :ensure nil
+;;   ;; :load-path "/usr/share/emacs/site-lisp/mu4e/"
+;;   ;; :defer 20 ; Wait until 20 seconds after startup
+;;   :config
+;; 
+;;   ;; This is set to 't' to avoid mail syncing issues when using mbsync
+;;   (setq mu4e-change-filenames-when-moving t)
+;; 
+;;   ;; Refresh mail using isync every 10 minutes
+;;   (setq mu4e-update-interval (* 10 60))
+;;   (setq mu4e-get-mail-command "mbsync -c '/home/hisbaan/.config/mu4e/mbsyncrc' -a")
+;;   ;; (setq mu4e-maildir "~/Mail/hisbaan-gmail")
+;;   (setq mu4e-root-maildir "~/Mail/hisbaan-gmail")
+;;   (setq my-mu4e-account-alist "hisbaan-gmail")
+;; 
+;;   (setq mu4e-drafts-folder "/[Gmail]/Drafts")
+;;   (setq mu4e-sent-folder   "/[Gmail]/Sent Mail")
+;;   (setq mu4e-refile-folder "/[Gmail]/All Mail")
+;;   (setq mu4e-trash-folder  "/[Gmail]/Trash")
+;; 
+;;   (setq mu4e-maildir-shortcuts
+;;       '(("/Inbox"             . ?i)
+;;         ("/[Gmail]/Sent Mail" . ?s)
+;;         ("/[Gmail]/Trash"     . ?t)
+;;         ("/[Gmail]/Drafts"    . ?d)
+;;         ("/[Gmail]/All Mail"  . ?a))))
 
 ;;;;;;;;;;
 ;; Misc ;;
