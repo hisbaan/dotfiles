667c5f254365c776e5c2c8f803dea96c250d239f
hisbaan <34548959+hisbaan@users.noreply.github.com>
2021-02-16 12:43:57 -0500

    Added some snippets, fixed unison configs

--- .doom.d/snippets/latex-mode/.yas-compiled-snippets.el
+++ .doom.d/snippets/latex-mode/.yas-compiled-snippets.el
@@ -7,14 +7,17 @@
                        ("mat" "\\begin{bmatrix} $0 \\end{bmatrix}" "matrix" nil nil nil "/home/hisbaan/.doom.d/snippets/latex-mode/matrix" nil nil)
                        ("mtemp" "\\documentclass[12pt]{article}\n\\usepackage[margin=1in]{geometry}\n\\usepackage{amsmath, amsthm, amssymb, graphicx, multicol, array, mathtools}\n\n\\DeclarePairedDelimiter\\autobracket{(}{)}\n\\newcommand{\\br}[1]{\\autobracket*{#1}}\n\\DeclarePairedDelimiter\\autosquarebracket{[}{]}\n\\newcommand{\\sbr}[1]{\\autosquarebracket*{#1}}\n\n\\newcommand{\\st}{\\text{ s.t. }}\n\\newcommand{\\R}{\\mathbb{R}}\n\\newcommand{\\Z}{\\mathbb{Z}}\n\\newcommand{\\N}{\\mathbb{N}}\n\\newcommand{\\Q}{\\mathbb{Q}}\n\n\\renewcommand{\\qedsymbol}{\\ensuremath{\\blacksquare}}\n\\renewcommand*{\\proofname}{\\underline{Pf}:}\n\\pagestyle{empty}\n\n\\title{TITLE}\n\\author{Hisbaan Noorani}\n\\date{DATE}\n\n\\begin{document}\n$0\n\\end{document}" "math-template" nil nil nil "/home/hisbaan/.doom.d/snippets/latex-mode/math-template" nil nil)
                        ("lalign" "\\begin{flalign} \\nonumber\n&\\begin{aligned}\n$0\n\\end{aligned} &&\n\\end{flalign}" "lalign" nil nil nil "/home/hisbaan/.doom.d/snippets/latex-mode/lalign" nil nil)
+                       ("itemize" "\\begin{itemize}\n\\item $0\n\\end{itemize}" "itemize" nil nil nil "/home/hisbaan/.doom.d/snippets/latex-mode/itemize" nil nil)
                        ("int" "\\int{$1} dx $0" "int_^" nil nil nil "/home/hisbaan/.doom.d/snippets/latex-mode/integral" nil nil)
                        ("frac" "\\frac{${1:numerator}}{${2:denominator}}$0" "frac-alt" nil nil nil "/home/hisbaan/.doom.d/snippets/latex-mode/frac-alt" nil nil)
                        ("/" "\\frac{${1:numerator}}{${2:denominator}}$0" "frac" nil nil nil "/home/hisbaan/.doom.d/snippets/latex-mode/frac" nil nil)
+                       ("enum" "\\begin{enumerate}\n\\item $0\n\\end{enumerate}" "enumerate" nil nil nil "/home/hisbaan/.doom.d/snippets/latex-mode/enumerate" nil nil)
                        ("e3" "\\vec{e_3}$0" "e3" nil nil nil "/home/hisbaan/.doom.d/snippets/latex-mode/e3" nil nil)
                        ("e2" "\\vec{e_2}$0" "e2" nil nil nil "/home/hisbaan/.doom.d/snippets/latex-mode/e2" nil nil)
                        ("e1" "\\vec{e_1}$0" "e1" nil nil nil "/home/hisbaan/.doom.d/snippets/latex-mode/e1" nil nil)
                        ("dint" "\\int_{${1:a}}^{${2:b}}{$3} dx $0" "def-int" nil nil nil "/home/hisbaan/.doom.d/snippets/latex-mode/def-integral" nil nil)
+                       ("apa" "\\usepackage[english]{babel}\n\\usepackage{csquotes}\n\\usepackage[style=apa]{biblatex}\n\\addbibresource{refs.bib}\n\\printbibliography[title={Reference}]" "apa" nil nil nil "/home/hisbaan/.doom.d/snippets/latex-mode/apa" nil nil)
                        ("align" "\\begin{align*}\n$0\n\\end{align*}" "align" nil nil nil "/home/hisbaan/.doom.d/snippets/latex-mode/align" nil nil)))
 
 
-;;; Do not edit! File generated at Thu Jan 28 14:45:50 2021
+;;; Do not edit! File generated at Thu Feb 11 14:51:06 2021
