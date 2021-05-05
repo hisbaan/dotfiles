7643ddf7ec6bd106eb70259c8888247274213ca4
hisbaan <34548959+hisbaan@users.noreply.github.com>
2021-05-04 21:02:23 -0400

    General update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Terminate already running bar instances
-killall -q polybar
-
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar mainbar-bspwm &
+if [[ $(pidof -s polybar) ]]
+then
+    polybar-msg cmd restart
+else
+    polybar mainbar-bspwm &
+fi
