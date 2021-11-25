a6b778e8e273eecf463b728b92e8297d5c270988
hisbaan <hisbaan@gmail.com>
2021-11-25 00:51:09 -0500

    Update nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,36 @@
+-- Packer
+require('plugins') -- lua/plugins.lua
+
+
+-- Vim settings
+vim.o.linebreak = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.incsearch = true
+vim.o.hidden = true
+vim.o.history = 5000
+vim.o.tabstop = 4
+vim.o.shiftwidth = vim.o.tabstop
+vim.g.mapleader = " "
+vim.o.clipboard = "unnamedplus"
+vim.wo.rnu = true
+vim.wo.number = true
+vim.o.mouse = "a"
+vim.o.encoding = "utf-8"
+vim.g.tex_flavor = "latex"
+
+-- Color scheme
+vim.g.rose_pine_variant = 'moon'
+vim.cmd('colorscheme rose-pine')
+
+-- Plugin configs
+require('lualine-config') 		-- lua/lualine-config.lua
+require('treesitter-config') 	-- lua/treesitter-config.lua
+require('nerdtree-config') 		-- lua/nerdtree-config.lua
+require('neogit-config')		-- lua/neogit-config.lua
+require('ultisnips-config')		-- lua/ultisnips-config.lua
+require('autopairs-config')		-- lua/autopairs-config
+require('orgmode-config')		-- lua/orgmode-config
+require('telescope-config')		-- lua/telescope-config
+
+-- neoscroll for smooth scrolling
---
+++ .config/nvim/lua/vimtex-config.lua
@@ -0,0 +1,4 @@
+vim.g.vimtex_view_method = "zathura"
+vim.g.vimtex_view_general_viewer = "zathura"
+vim.g.vimtex_general_view_options = "--unique file:@pdf\#src:@line@tex"
+vim.g.vimtex_view_general_options_latexmk = "--unique"
