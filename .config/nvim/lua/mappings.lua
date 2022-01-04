553b97f66c5ff10c487936fb5f9b06480136204f
hisbaan <hisbaan@gmail.com>
2022-01-03 22:37:12 -0500

    Update nvim and add alacritty

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -52,7 +52,7 @@ map('n', '<Leader>f', '<Cmd>NvimTreeToggle<CR>', default_options )
 
 -- Telescope
 map('n', '<Leader><Space>', '<Cmd>Telescope find_files<CR>', default_options)
-map('n', '<Leader>.', '<Cmd>Telescope file_browser<CR>', default_options)
+map('n', '<Leader>.', "<Cmd>lua require 'telescope'.extensions.file_browser.file_browser()<CR>", default_options)
 
 -- Terminal
 map('n', '<Leader>tt', '<Cmd>terminal<CR>A', default_options)
